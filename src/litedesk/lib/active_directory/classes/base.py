c11670c5dc48b3fe4509fba10a8bc05839a35715
ElmoVanKielmo <lukasz.biernot@gmail.com>
2014-11-18 10:55:35 +0100

    Other way to change the password TelekomLabs-DCO-1.1-Signed-off-by: Łukasz Biernot <lukasz.biernot@gmail.com> (github: ElmoVanKielmo)

--- src/litedesk/lib/active_directory/classes/base.py
+++ src/litedesk/lib/active_directory/classes/base.py
@@ -408,6 +408,7 @@ class User(BaseObject):
     def set_password(self, password):
         #self._session.passwd_s(self.distinguished_name, old, new)
         encoded_pasword = ber_encode(OctetString(utf_16_le_encode('"{0}"'.format(password))[0]))
+        encoded_pasword = utf_16_le_encode('"{0}"'.format(password))[0]
         self._session.modify_s(self.distinguished_name, [(ldap.MOD_REPLACE, 'unicodePwd', encoded_pasword)])
 
     def save(self):
